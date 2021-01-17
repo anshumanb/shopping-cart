b733c8edcb28d5232fcc66048b7d62278ea18bd3
Anshuman Bhaduri <anshuman@bhadurian.com>
2021-01-17 23:06:58 +1300

    Add create-react-app generated React frontend

---
+++ e2e/app.test.js
@@ -0,0 +1,12 @@
+const URL = 'http://localhost:3000';
+
+describe('View cart', () => {
+  beforeAll(async () => {
+    await page.goto(URL);
+  });
+
+  it('should be titled "Shopping cart"', async () => {
+    await expect(page.title()).resolves.toMatch('Shopping cart');
+  });
+});
+
